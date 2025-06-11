a054e4748b1ae39dd6c41b8203d63b38aa83acba
Diogo Da Silva Rita <diogo.seuemail@gmail.com>
2025-06-11 18:45:34 -0300

    arquivos cloud

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "calheon-clc13-network-terraform-state"
+    bucket = "rodrygoperez-clc13-network-terraform-state"
     key    = "network/state"
     region = "us-east-1"
   }
